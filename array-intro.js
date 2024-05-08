b2dd37a0ff5e07ae9aed9e02c4558a6485a6da29
Samuel <samglz10@gmail.com>
2024-05-07 21:33:25 -0400

    02 exercise passed 1 of 7 tests

---
+++ array-intro.js
@@ -0,0 +1,11 @@
+//https://www.youtube.com/watch?v=7W4pQQ20nJg
+//starts at index 0; length of array a is 4; 
+const a = ["A","B","C","D"];
+a.push("E");
+a.push("F")
+
+console.log(a);
+
+a.push([1,2]);
+
+console.log(a);
