efaaedc5b43ed8512a9f85c7f0796962b1e6bbfe
Samuel <samglz10@gmail.com>
2024-05-07 21:58:25 -0400

    03 adn 04 passes first tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, query) {
+    const filteredArray = [];
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] !== query){
+        filteredArray.push(arr[i])
+    }
+  }
+    return filteredArray
 };
 
 // Do not edit below this line
