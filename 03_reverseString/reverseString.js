df50ac4739c346b86a4d7a77cc7a7d3752c27e05
Samuel <samglz10@gmail.com>
2024-05-08 20:20:21 -0400

    05 and06 passing inital test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const reversedArray = []
+    for(let i = 0; i < string.length; i++){
+        reversedArray.push(string[i])
+    }
+    return reversedArray.reverse();
 };
 
 // Do not edit below this line
