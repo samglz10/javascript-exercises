3b915f4df48ed6b12126b2742c9672b55706d6e2
Samuel <samglz10@gmail.com>
2024-05-10 14:59:56 -0400

    10 fib tests passed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if(num < 0){
+        return 'OOPS'
+    }
+    const sequence = [0,1];
+    for(let i = 2; i <= num; i++){
+        //F(n) = F(n-1) + F(n-2)
+        sequence[i] = sequence[i - 1] + sequence[i - 2];
+    }
+    return sequence[num]
 };
 
+fibonacci(4); // returns the 4th member of the series: 3  (1, 1, 2, 3)
+console.log('\n')
+fibonacci(6); // returns 8
 // Do not edit below this line
 module.exports = fibonacci;
