cc2d24f27e407ebae65f66be66e378ec7ca6ac81
Samuel <samglz10@gmail.com>
2024-05-12 15:06:58 -0400

    getTitles working

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,22 @@
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
 const getTheTitles = function() {
-
+  const bookTitles = []
+  for(let i =0; i < books.length; i ++){
+   bookTitles.push(books[i].title) ;
+  }
+  return bookTitles;
 };
+
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
