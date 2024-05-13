55d490bfc4f299c0838bd79eb2abf3ce17414f3a
Samuel <samglz10@gmail.com>
2024-05-12 20:31:18 -0400

    getting find the oldert

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,49 @@
-const findTheOldest = function() {
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+      yearOfDeath: 1970,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
 
-};
 
+const findTheOldest = function(people) {
+
+    const getAge=(death, birth) =>{
+        if(death === undefined){
+             death = new Date().getFullYear();
+             return death - birth;
+        }
+        return death - birth;
+
+    }
+    let eldest = {
+        name: "",
+        age: 0
+    };
+
+    people.map((person)=>{
+        let getAgeFunction = getAge(person.yearOfDeath, person.yearOfBirth);
+        if(getAgeFunction > eldest.age && getAgeFunction!== undefined ){
+            eldest = {
+                name: person.name,
+                age: getAgeFunction
+            }
+        }
+        
+    })
+    return eldest
+}
+findTheOldest(people)
 // Do not edit below this line
 module.exports = findTheOldest;
