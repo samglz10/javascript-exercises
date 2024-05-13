41c3ad22b92f2b7743e726681719d23ebd409e29
Samuel <samglz10@gmail.com>
2024-05-12 20:32:15 -0400

    edited findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,20 +1,3 @@
-const people = [
-    {
-      name: "Carly",
-      yearOfBirth: 1942,
-      yearOfDeath: 1970,
-    },
-    {
-      name: "Ray",
-      yearOfBirth: 1962,
-      yearOfDeath: 2011,
-    },
-    {
-      name: "Jane",
-      yearOfBirth: 1912,
-      yearOfDeath: 1941,
-    },
-  ]
 
 
 const findTheOldest = function(people) {
@@ -42,8 +25,8 @@ const findTheOldest = function(people) {
         }
         
     })
-    return eldest
+    return eldest;
 }
-findTheOldest(people)
+
 // Do not edit below this line
 module.exports = findTheOldest;
